df58fd114a95a3469c7cfc94887fde9d03da3003
Evellyn Karla <evellynkarla1418@gmail.com>
2024-07-02 16:43:23 -0300

    exercicios

---
+++ basico/src/basico4/exercicio.java
@@ -0,0 +1,63 @@
+package basico4;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class exercicio {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// removendo espaços 
+		String texto = " Texto para retirar espaços do inicio e do fim ";
+		
+		String removido = texto.trim();
+		
+		System.out.println(removido);
+
+		// ------------------------------
+		
+		// Contagem de palavras 
+		
+//		System.out.println("Digite a frase: ");
+//		String frase = sc.nextLine();
+//		
+//		StringTokenizer exe = new StringTokenizer(frase);
+//		
+//		int tam = exe.countTokens();
+//		
+//		System.out.println("A frase - " + frase + " - possui " + tam + " palavras" );
+		// ---------------------------------------------------------------------
+		
+		// Comparando textos 
+		
+//		String texto1 = "Os textos são iguais?";
+//		String texto2 = "Os textos são iguais?";
+//		
+//		boolean igual = texto1.equals(texto2);
+//		if(igual) {
+//			System.out.println("São iguais.");
+//		}else {
+//			System.out.println("Não são iguais.");
+//		}
+	
+		// ----------------------------------
+		
+		// Texto para maiusculos
+		
+//		System.out.println("Digite a frase: ");
+//		String frase2 = sc.nextLine();
+//		
+//		System.out.println(frase2.toUpperCase());
+		
+		// array para texto maiusculo
+		
+		String[] palavras = {"a", "vida", "é", "bela"};
+		
+		String pratexto = (Arrays.toString(palavras)).replace(",", "").replace("[", "").replace("]", "");
+		
+		System.out.println(pratexto.toUpperCase());
+	}
+}
--- basico/src/basico4/stringMetodos.java
+++ basico/src/basico4/stringMetodos.java
@@ -1,23 +1,25 @@
 package basico4;
 
+import java.util.StringTokenizer;
+
 public class stringMetodos {
 
 	public static void main(String[] args) {
-		String str = "Hello Word";
-		String str1 = " hello word ";
+		String str = "Hello World";
+		String str1 = " hello world ";
 		
-		String resultado1 = str.substring(6); // "Word"
-		String resultado2 = str.substring(3, 8); // "lo Word"
+		String resultado1 = str.substring(6); // "World"
+		String resultado2 = str.substring(3, 8); // "lo World"
 		
 		System.out.println(resultado1 + " || " + resultado2);
 	
 		
-		String resultado3 = str.toUpperCase(); // "HELLO WORD"
-		String resultado4 = str.toLowerCase(); // "hello word"
+		String resultado3 = str.toUpperCase(); // "HELLO WORlD"
+		String resultado4 = str.toLowerCase(); // "hello world"
  
 		System.out.println(resultado3 + " || " + resultado4);
 
-		String resultado5 = str1.trim(); // "hello word"
+		String resultado5 = str1.trim(); // "hello world"
 
 		
 		System.out.println(str1 + "||" + resultado5);
@@ -36,9 +38,23 @@ public class stringMetodos {
 		
 		int tam = str.length(); // 10
 		
-		System.out.println(tam);
+		String dois = "Hello World World2";
 		
+		int posi = dois.indexOf("l"); // 2
+		System.out.println(posi); 
 		
+		posi = dois.lastIndexOf("l"); // 15
+		System.out.println(posi); 
+		
+		String valor = "CDD - Java";
+		
+		System.out.println(valor.endsWith("Java")); // true
+		System.out.println(valor.startsWith("C")); // true
+		System.out.println(valor.startsWith("DD", 1)); // true, compara a partir do 1
+		
+		StringTokenizer exe = new StringTokenizer("Não iremos desistir nunca.");
+		
+		System.out.println(exe.countTokens()); // 4
 		
 		
 		
